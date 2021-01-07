32dc1709b9a5ff33602c10d29561823acc8bc4c7
Hi-im-Yan <yanajiki2010@hotmail.com>
2021-01-06 22:10:09 -0400

    HTML style updated

--- project/app/forms.py
+++ project/app/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class Location(forms.Form):
     name = forms.CharField(max_length=100, label='Nome', widget=forms.TextInput(
-        attrs={'placeholder': 'Cidade/Estado/Pais'}))
+        attrs={'placeholder': 'Cidade/Estado/Pais', 'class': 'form-control col-4'}))
